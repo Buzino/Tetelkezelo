a6a460d8fb59410b792012149c67c2abd207eed0
Buzino <160595331+Buzino@users.noreply.github.com>
2024-06-03 20:26:52 +0200

    Lerolerolerolero

--- weboldal/modify.php
+++ weboldal/modify.php
@@ -17,21 +17,42 @@
         <main>
             <nav>
                 <h2>Menü</h2>
-                <a href="">Új tétel</a>
                 <a href="">Kézikönyv</a>
             </nav>
             <form action="" method="post" class="keres modosit">
               <fieldset>
                 <legend>Tétel hozzáadása / módosítása</legend>
+                <label for="tantargy">Tantárgy:</label>
+                <input type="text" name="tantargy" id="tantargy" list="targy">
+                <datalist name="targy" id="targy">
+                    <?php 
+                        $result = $conn->query("SELECT nev FROM targyak ORDER BY nev ASC")->fetch_all(MYSQLI_ASSOC);
+                        foreach ($result as $targy):
+                    ?>
+                    <option value="<?=$targy["nev"]?>" <?php if (($_POST["targy"] ?? false) == $targy["nev"]) echo "selected"?>></option>
+                    <?php endforeach;?>
+                </datalist>
+                <br>
+                <label for="cim">Tétel címe:</label> <input type="text" name="cim" id="cim" value="<?=$_POST["cim"] ?? ""?>">
+                <br>
+                <label for="vazlat">Vázlat:</label> <textarea name="vazlat" id="vazlat" rows="5"><?=$_POST["vazlat"] ?? ""?></textarea>
+                <br>
+                <label for="kidolgozas">Kidolgozás:</label> <textarea name="kidolgozas" id="kidolgozas" rows="5"><?=$_POST["vazlat"] ?? ""?></textarea>
+                
                 <div class="modositgombok">
                     <button type="submit" name="modosit" value="1">Mentés</button>
-                    <button type="submit" name="megsem" value="1">Mégsem</button>
+                    <button type="button" name="megsem" value="1" onclick="elvetes()">Mégsem</button>
                 </div>
-                <label for="szoveg">Keresés szövege:</label> <input type="text" name="szoveg" id="szoveg" value="<?=($_POST["szoveg"] ?? '')?>">
-                <br>
               </fieldset>
             </form>
         </main>
     </div>
+    <script defer>
+        function elvetes() {
+            if (confirm("Biztosan kilép?\nA módosítások elvetésre kerülnek.")) {
+                window.open("index.php", "_self");
+            }
+        }
+    </script>
 </body>
 </html>
