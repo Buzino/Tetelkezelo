61e20196b8b90209aa1d58773be9f4bb3a7631a3
Buzino <160595331+Buzino@users.noreply.github.com>
2024-06-03 22:00:54 +0200

    Fortnite

--- weboldal/modify.php
+++ weboldal/modify.php
@@ -1,5 +1,9 @@
 <?php 
     include("php/db_connect.php");
+
+    if (isset($_POST["modosit"]) && isset($_POST["tetelid"])) {
+        $tetel = $conn->query("SELECT tetelek.id AS tetelid, targyak.nev AS targy, sorszam, cim, vazlat, kidolgozas FROM tetelek INNER JOIN targyak ON tetelek.tantargyid = targyak.id WHERE tetelek.id = ".$_POST["tetelid"])->fetch_assoc();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -23,27 +27,37 @@
               <fieldset>
                 <legend>Tétel hozzáadása / módosítása</legend>
                 <label for="tantargy">Tantárgy:</label>
-                <input type="text" name="tantargy" id="tantargy" list="targy">
+                <input type="text" name="tantargy" id="tantargy" list="targy" value="<?=$tetel["targy"] ?? $_POST["tantargy"] ?? ""?>" required>
                 <datalist name="targy" id="targy">
                     <?php 
                         $result = $conn->query("SELECT nev FROM targyak ORDER BY nev ASC")->fetch_all(MYSQLI_ASSOC);
                         foreach ($result as $targy):
                     ?>
-                    <option value="<?=$targy["nev"]?>" <?php if (($_POST["targy"] ?? false) == $targy["nev"]) echo "selected"?>></option>
+                    <option><?=$targy["nev"]?></option>
                     <?php endforeach;?>
                 </datalist>
                 <br>
-                <label for="cim">Tétel címe:</label> <input type="text" name="cim" id="cim" value="<?=$_POST["cim"] ?? ""?>">
+                <label for="sorszam">Tétel sorszáma:</label> <input type="number" min=1 name="sorszam" id="sorszam" value="<?=$tetel["sorszam"] ?? $_POST["sorszam"] ?? ""?>" required>
                 <br>
-                <label for="vazlat">Vázlat:</label> <textarea name="vazlat" id="vazlat" rows="5"><?=$_POST["vazlat"] ?? ""?></textarea>
+                <label for="cim">Tétel címe:</label> <input type="text" name="cim" id="cim" value="<?=$tetel["cim"] ?? $_POST["cim"] ?? ""?>" required>
                 <br>
-                <label for="kidolgozas">Kidolgozás:</label> <textarea name="kidolgozas" id="kidolgozas" rows="5"><?=$_POST["vazlat"] ?? ""?></textarea>
+                <label for="vazlat">Vázlat:</label> <textarea name="vazlat" id="vazlat" rows="5" required><?=$tetel["vazlat"] ?? $_POST["vazlat"] ?? ""?></textarea>
+                <br>
+                <label for="kidolgozas">Kidolgozás:</label> <textarea name="kidolgozas" id="kidolgozas" rows="5" required><?=$tetel["kidolgozas"] ?? $_POST["kidolgozas"] ?? ""?></textarea>
                 
                 <div class="modositgombok">
-                    <button type="submit" name="modosit" value="1">Mentés</button>
+                    <button type="submit" name="ment" value="1" onclick="mentes()">Mentés</button>
                     <button type="button" name="megsem" value="1" onclick="elvetes()">Mégsem</button>
                 </div>
               </fieldset>
+            <?php 
+            if (!isset($_POST["tetelid"])) {
+                $tetelid = intval($conn->query("SELECT MAX(id) AS maxid FROM tetelek;")->fetch_assoc()["maxid"]) + 1;
+            } else {
+                $tetelid = $tetel["tetelid"] ?? $_POST["tetelid"];
+            }
+            ?>
+            <input type="hidden" name="tetelid" value="<?=$tetelid?>">
             </form>
         </main>
     </div>
@@ -54,5 +68,19 @@
             }
         }
     </script>
+    <?php
+    if (isset($_POST["ment"])) {
+        switch( intval($conn->query("SELECT COUNT(*) AS db FROM targyak WHERE nev = '".$_POST["tantargy"]."'")->fetch_assoc()["db"]) ) {
+            case 0:
+                $targyid = intval( $conn->query("SELECT MAX(id) AS maxid FROM targyak")->fetch_assoc()["maxid"] ) + 1;
+                $conn->query("INSERT INTO targyak VALUES ($targyid, '".$_POST["tantargy"]."')");
+                break;
+            default:
+                $targyid = intval( $conn->query("SELECT id FROM targyak WHERE nev = '".$_POST["tantargy"]."'")->fetch_assoc()["id"] );
+                break;
+        }
+        $conn->query("REPLACE INTO tetelek (id, tantargyid, sorszam, cim, vazlat, kidolgozas) VALUES (".$_POST["tetelid"].", ".$targyid.", ".$_POST["sorszam"].", '".$_POST["cim"]."', '".$_POST["vazlat"]."', '".$_POST["kidolgozas"]."')");
+    }
+    ?>
 </body>
 </html>
